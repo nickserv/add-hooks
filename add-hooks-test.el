9a04dbedcabfd57d1827f23cb6c4b44619201531
Nick McCurdy <nick@nickmccurdy.com>
2017-04-07 07:54:05 -0400

    Add unit tests using ERT

---
+++ add-hooks-test.el
@@ -0,0 +1,42 @@
+;;; add-hooks-test.el --- Tests for add-hooks
+
+;;; Code:
+
+(require 'ert)
+(require 'add-hooks)
+
+(ert-deftest add-hooks-listify-test ()
+  (should (equal (add-hooks-listify nil) nil))
+  (should (equal (add-hooks-listify '(1)) '(1)))
+  (should (equal (add-hooks-listify 1) '(1))))
+
+(ert-deftest add-hooks-mapflat-test ()
+  (should (equal (add-hooks-mapflat #'reverse
+                                    '((1 2) (3 4)))
+                 '(2 1 4 3))))
+
+(ert-deftest add-hooks-test ()
+  (should (equal (macroexpand '(add-hooks (1 . 2)))
+                 '(add-hook '1 '2)))
+  (should (equal (macroexpand '(add-hooks (1 . 2)
+                                          (3 . 4)))
+                 '(progn
+                    (add-hook '1 '2)
+                    (add-hook '3 '4))))
+  (should (equal (macroexpand '(add-hooks (1 . (2 3))))
+                 '(progn
+                    (add-hook '1 '2)
+                    (add-hook '1 '3))))
+  (should (equal (macroexpand '(add-hooks ((1 2) . 3)))
+                 '(progn
+                    (add-hook '1 '3)
+                    (add-hook '2 '3))))
+  (should (equal (macroexpand '(add-hooks ((1 2) . (3 4))))
+                 '(progn
+                    (add-hook '1 '3)
+                    (add-hook '1 '4)
+                    (add-hook '2 '3)
+                    (add-hook '2 '4)))))
+
+(provide 'add-hooks-test)
+;;; add-hooks-test.el ends here
