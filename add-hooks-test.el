819769834e1788af3da801afecd7dc1c3cc840e6
Nick McCurdy <nick@nickmccurdy.com>
2017-04-07 09:27:49 -0400

    Shorten test names

--- add-hooks-test.el
+++ add-hooks-test.el
@@ -5,17 +5,17 @@
 (require 'ert)
 (require 'add-hooks)
 
-(ert-deftest add-hooks-listify-test ()
-  (should (equal (add-hooks-listify nil) nil))
+(ert-deftest add-hooks-listify ()
+  (should (eq (add-hooks-listify nil) nil))
   (should (equal (add-hooks-listify '(1)) '(1)))
   (should (equal (add-hooks-listify 1) '(1))))
 
-(ert-deftest add-hooks-mapflat-test ()
+(ert-deftest add-hooks-mapflat ()
   (should (equal (add-hooks-mapflat #'reverse
                                     '((1 2) (3 4)))
                  '(2 1 4 3))))
 
-(ert-deftest add-hooks-test ()
+(ert-deftest add-hooks ()
   (should (equal (macroexpand '(add-hooks (1 . 2)))
                  '(add-hook '1 '2)))
   (should (equal (macroexpand '(add-hooks (1 . 2)
