4327de4a8615e542cbbf24c17fc6b87f710852b4
Nick McCurdy <nick@nickmccurdy.com>
2017-05-16 23:02:51 -0400

    Rename hook variables in tests, making them more standardized

--- add-hooks-test.el
+++ add-hooks-test.el
@@ -14,49 +14,49 @@
 (macrolet ((fixture
             (&rest body)
             "Declare hook variables locally, then eval BODY forms."
-            `(let ((hook-a) (hook-b))
+            `(let ((a-hook) (b-hook))
                ,@body)))
 
   (ert-deftest add-hooks-pair-one-to-one ()
-    (fixture (add-hooks-pair 'hook-a 'a)
-             (should (equal hook-a '(a)))))
+    (fixture (add-hooks-pair 'a-hook 'a)
+             (should (equal a-hook '(a)))))
 
   (ert-deftest add-hooks-pair-one-to-many ()
-    (fixture (add-hooks-pair 'hook-a '(a b))
-             (should (equal hook-a '(b a)))))
+    (fixture (add-hooks-pair 'a-hook '(a b))
+             (should (equal a-hook '(b a)))))
 
   (ert-deftest add-hooks-pair-many-to-one ()
-    (fixture (add-hooks-pair '(hook-a hook-b) 'a)
-             (should (equal hook-a '(a)))
-             (should (equal hook-b '(a)))))
+    (fixture (add-hooks-pair '(a-hook b-hook) 'a)
+             (should (equal a-hook '(a)))
+             (should (equal b-hook '(a)))))
 
   (ert-deftest add-hooks-pair-many-to-many ()
-    (fixture (add-hooks-pair '(hook-a hook-b) '(a b))
-             (should (equal hook-a '(b a)))
-             (should (equal hook-b '(b a)))))
+    (fixture (add-hooks-pair '(a-hook b-hook) '(a b))
+             (should (equal a-hook '(b a)))
+             (should (equal b-hook '(b a)))))
 
   (ert-deftest add-hooks-one-to-one ()
-    (fixture (add-hooks '((hook-a . a)))
-             (should (equal hook-a '(a)))))
+    (fixture (add-hooks '((a-hook . a)))
+             (should (equal a-hook '(a)))))
 
   (ert-deftest add-hooks-multiple ()
-    (fixture (add-hooks '((hook-a . a) (hook-b . b)))
-             (should (equal hook-a '(a)))
-             (should (equal hook-b '(b)))))
+    (fixture (add-hooks '((a-hook . a) (b-hook . b)))
+             (should (equal a-hook '(a)))
+             (should (equal b-hook '(b)))))
 
   (ert-deftest add-hooks-one-to-many ()
-    (fixture (add-hooks '((hook-a . (a b))))
-             (should (equal hook-a '(b a)))))
+    (fixture (add-hooks '((a-hook . (a b))))
+             (should (equal a-hook '(b a)))))
 
   (ert-deftest add-hooks-many-to-one ()
-    (fixture (add-hooks '(((hook-a hook-b) . a)))
-             (should (equal hook-a '(a)))
-             (should (equal hook-b '(a)))))
+    (fixture (add-hooks '(((a-hook b-hook) . a)))
+             (should (equal a-hook '(a)))
+             (should (equal b-hook '(a)))))
 
   (ert-deftest add-hooks-many-to-many ()
-    (fixture (add-hooks '(((hook-a hook-b) . (a b))))
-             (should (equal hook-a '(b a)))
-             (should (equal hook-b '(b a))))))
+    (fixture (add-hooks '(((a-hook b-hook) . (a b))))
+             (should (equal a-hook '(b a)))
+             (should (equal b-hook '(b a))))))
 
 (provide 'add-hooks-test)
 ;;; add-hooks-test.el ends here
