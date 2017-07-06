edd4cb032a509b576d88f4cc0521ebfe66a9e6c7
Nick McCurdy <nick@nickmccurdy.com>
2017-07-06 01:05:54 -0400

    Version 3.1.0

--- add-hooks.el
+++ add-hooks.el
@@ -4,7 +4,7 @@
 
 ;; Author: Nick McCurdy <nick@nickmccurdy.com>
 ;; Created: 22 Jan 2017
-;; Version: 3.0.0
+;; Version: 3.1.0
 ;; Keywords: lisp
 ;; Homepage: https://github.com/nickmccurdy/add-hooks
 
